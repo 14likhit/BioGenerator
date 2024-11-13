f018098057b2b39e8b5fc6ce629e81db2455a8f1
14likhit <14likhit@gmail.com>
2024-11-13 21:39:55 +0530

    [FEAT]: Integrated Gemini API.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
+    alias(libs.plugins.secrets.gradle.plugin)
 }
 
 android {
@@ -38,6 +43,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -59,6 +65,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.vertexai)
+    implementation(libs.firebase.crashlytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +74,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.generativeai)
 }
--- app/src/main/java/com/date/biogenerator/MainActivity.kt
+++ app/src/main/java/com/date/biogenerator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.date.biogenerator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +12,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.date.biogenerator.ui.theme.BioGeneratorTheme
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val generativeModel =
+            GenerativeModel(
+                // Specify a Gemini model appropriate for your use case
+                modelName = "gemini-1.5-flash",
+                // Access your API key as a Build Configuration variable (see "Set up your API key" above)
+                apiKey = BuildConfig.apiKey
+            )
+
+        val prompt = "Write a story about a magic backpack."
+        lifecycleScope.launch {
+            val response = generativeModel.generateContent(prompt)
+            Log.e("Test","Response ${response.text}")
+        }
         setContent {
             BioGeneratorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
